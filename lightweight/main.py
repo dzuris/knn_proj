2ca2747be778dae3836975e9bbc67fee53379035
JohnyDevp <holan.jan@outlook.com>
2024-05-01 13:46:10 +0200

    added optimizer

--- lightweight/main.py
+++ lightweight/main.py
@@ -13,13 +13,12 @@ import torch.nn as nn
 import torch.utils.data as td
 import torchvision.models as models
 from torchvision import transforms
+from tqdm import tqdm
 
 # some definitions necessary to be global
-#### Transformation augmentation
-
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-def getLeightWeightModel():
+def getLeightweightModel():
     # model = torch.hub.load('XingangPan/IBN-Net', 'resnet50_ibn_a', pretrained=True)
     # download mobilenet and set its weights
     model = models.mobilenet_v2(weights=models.MobileNet_V2_Weights.IMAGENET1K_V2) # torch.hub.load('pytorch/vision:v0.10.0', 'mobilenet_v2', pretrained=True)
@@ -28,21 +27,28 @@ def getLeightWeightModel():
     print('Number of parameters: ', sum(p.numel() for p in model.parameters()))
     return model
 
-def getTeacherModel(weight_path, type='MBR_4G'):
+def getTeacherModel(weight_path, model_type='MBR_4G'):
+    """
+    Args:
+        weight_path : path to the file *.pt with weights for the model_type
+        model_type : type of the used model [MBR_4G|MBR_4B]
+    """
     # get model according to the type 
-    model = eval.get_model(type, device)
+    model = eval.get_model(model_type, device)
     model.load_state_dict(torch.load(weight_path, map_location='cpu')) 
     model = model.to(device)
-    model.eval() # this model will always just produce results, not to be trained
+    model.eval() # this model will always just produce results, it wont be trained
 
-def getDatasetInParts(path, dataset='VERI-Wild'):
+def getDatasetInParts(path, data, dataset='VERI-Wild'):
     """
     Args:
         path : path to the root folder of the dataset (not to exact files!)
         dataset : [VERI-Wild|VehicleID|Veri776]
+    Return:
+        data_train, data_g, data_q
     """
-    batch_size = 32
-    #### Dataset Loading       
+    
+    ## Dataset Loading       
     if dataset == "VehicleID":
         data_q = trs.CustomDataSet4VehicleID(path+'/train_test_split/test_list_800.txt', path+'/image/', is_train=False, mode="q", transform=test_transform)
         data_g = trs.CustomDataSet4VehicleID(path+'/train_test_split/test_list_800.txt', path+'/image/', is_train=False, mode="g", transform=test_transform)
@@ -52,8 +58,8 @@ def getDatasetInParts(path, dataset='VERI-Wild'):
         data_g = trs.DataLoader(data_g, batch_size=data['BATCH_SIZE'], shuffle=False, num_workers=data['num_workers_teste'])
     if dataset == 'VERIWILD':
         data_q = trs.CustomDataSet4VERIWILD(path+'/train_test_split/test_3000_id_query.txt', path+'/images/', transform=test_transform, with_view=False)
-        data_g = trs.CustomDataSet4VERIWILD('/home/eurico/VERI-Wild/train_test_split/test_3000_id.txt', path+'/images/', transform=test_transform, with_view=False)
-        data_train = trs.CustomDataSet4VERIWILD('/home/eurico/VERI-Wild/train_test_split/train_list.txt', path+'/images/', transform=train_transform, with_view=False)
+        data_g = trs.CustomDataSet4VERIWILD(path+'/train_test_split/test_3000_id.txt', path+'/images/', transform=test_transform, with_view=False)
+        data_train = trs.CustomDataSet4VERIWILD(path+'/train_test_split/train_list.txt', path+'/images/', transform=train_transform, with_view=False)
         data_train = td.DataLoader(data_train, sampler=trs.RandomIdentitySampler(data_train, data['BATCH_SIZE'], data['NUM_INSTANCES']), num_workers=data['num_workers_train'], batch_size = data['BATCH_SIZE'], collate_fn=trs.train_collate_fn, pin_memory=True)
         data_q = td.DataLoader(data_q, batch_size=data['BATCH_SIZE'], shuffle=False, num_workers=data['num_workers_teste'])
         data_g = td.DataLoader(data_g, batch_size=data['BATCH_SIZE'], shuffle=False, num_workers=data['num_workers_teste'])
@@ -68,7 +74,15 @@ def getDatasetInParts(path, dataset='VERI-Wild'):
         data_train = td.DataLoader(data_train, sampler=trs.RandomIdentitySampler(data_train, data['BATCH_SIZE'], data['NUM_INSTANCES']), num_workers=data['num_workers_train'], batch_size = data['BATCH_SIZE'], collate_fn=trs.train_collate_fn, pin_memory=True)
         data_g = td.DataLoader(data_g, batch_size=data['BATCH_SIZE'], shuffle=False, num_workers=data['num_workers_teste'])
         data_q = td.DataLoader(data_q, batch_size=data['BATCH_SIZE'], shuffle=False, num_workers=data['num_workers_teste'])
- 
+
+    return data_train, data_g, data_q
+
+def do_training(m_student, m_teacher, dataloader, num_epochs):
+    optimizer = torch.optim.Adam(lr=0.0001)
+    
+    for epoch in range(num_epochs):
+        for image_batch, label, cam, view in tqdm(dataloader, desc='Epoch ' + str(epoch+1) +' (%)' , bar_format='{l_bar}{bar:20}{r_bar}'): 
+            
 if __name__ == '__main__':
     # setup
     data = {
@@ -76,7 +90,12 @@ if __name__ == '__main__':
         'x_length':256,
         
         'n_mean': [0.5, 0.5, 0.5],
-        'n_std': [0.5, 0.5, 0.5]
+        'n_std': [0.5, 0.5, 0.5],
+        
+        'BATCH_SIZE' : 32,
+        'NUM_INSTANCES' : 4,
+        'num_workers_teste': 16,
+        'num_workers_train': 8
     }
     # ======
     
